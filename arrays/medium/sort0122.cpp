d7af27459ec1ac57a51c4c1a4024ba4b2dcd1381
Rishika Kinger <rishikakinger@gmail.com>
2023-04-15 09:49:31 +0530

    finished sort an array of 0's 1's and 2's

--- arrays/medium/sort0122.cpp
+++ arrays/medium/sort0122.cpp
@@ -8,4 +8,31 @@
 
 #include<bits/stdc++.h>
 #include<iostream>
+#include<vector>
 using namespace std;
+
+
+void sortArray(vector<int>& arr, int n){
+    int low=0, mid=0, high=n-1;
+    while(mid<=high){//till we have unsorted section
+        if(arr[mid]==1){
+            mid++;
+        }
+        else if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            mid++;
+            low++;
+        }
+        else{
+            swap(arr[high], arr[mid]);
+            high--;
+        }
+
+
+    }
+
+}
+
+
+//time complexity is O(N)
+//space complexity O(1)
