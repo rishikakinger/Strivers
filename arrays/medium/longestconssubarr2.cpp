1265081e1a84f5945b584c172d44d145355fee1c
rishikakinger <rishikakinger@email.com>
2023-05-30 12:43:16 +0530

    longest consecutive subarray done

--- arrays/medium/longestconssubarr2.cpp
+++ arrays/medium/longestconssubarr2.cpp
@@ -1,7 +1,5 @@
 //optimal solution 
-//optimal only under some constraints
-
-
+//unordered set
 
 
 
@@ -10,5 +8,34 @@
 #include<iostream>
 using namespace std;
 
+class Solution{
+  public:
 
-
+    int findLongestConseqSubseq(int nums[], int n)
+    {
+        if(n==0) return 0;
+        int longest=1;
+        unordered_set<int> st;
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+        
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                int cnt=1;
+                int x=it;
+                while(st.find(x+1)!=st.end()){
+                    x++;
+                    cnt++;
+                    
+                }
+                longest=max(longest,cnt);
+            }
+        }
+        
+        
+        
+        
+        return longest;
+    }
+};
