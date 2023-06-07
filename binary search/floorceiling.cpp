69b412941c1ececfc8ad4c0232368018436919c4
rishikakinger <rishikakinger@gmail.com>
2023-06-07 13:28:08 +0530

    done with floor ceiling

---
+++ binary search/floorceiling.cpp
@@ -0,0 +1,17 @@
+/*
+
+FLOOR- largest no in the array <=x
+
+if(a[mid]<=x){
+    ans=ans[mid]
+    low=mid+1
+}
+
+
+
+
+CEILING- smallest no in the array >=x
+       - same as lower bound except return -1 if doesnt exist
+
+
+*/
