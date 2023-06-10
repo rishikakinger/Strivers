c0a9008e6012c0bc63970073e67f7e48b356bf6c
rishikakinger <rishikakinger@gmail.com>
2023-06-10 22:10:36 +0530

    smallest divisor done

--- binary search/search space/smallestdivisor.cpp
+++ binary search/search space/smallestdivisor.cpp
@@ -4,16 +4,29 @@
 using namespace std;
 
 
-bool check(vector<int>&v){
+double check(vector<int>&v, int mid){
+    double sum=0;
+    for(int i=0;i<v.size();i++){
+        sum+=ceil((double)(v[i]/mid)); //put brackets around double dumbass
+    }
+    return sum;
+
 
 }
 
 
-
-int minDays(vector<int>& bloomDay, int m, int k){
+int smallestDivisor(vector<int>& v, int k){
     int low=0,high=INT_MAX-1;
     int ans=-1;
     while(low<=high){
+        int mid=low+(high-low)/2;
+        if(check(v,mid)<=k){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
 
     }
     return ans;
