6d9466dcc6c029c681dc8da3f66a20474ebc1f15
rishikakinger <rishikakinger@gmail.com>
2023-06-10 20:49:32 +0530

    started smallest divisor

---
+++ binary search/search space/smallestdivisor.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+bool check(vector<int>&v){
+
+}
+
+
+
+int minDays(vector<int>& bloomDay, int m, int k){
+    int low=0,high=INT_MAX-1;
+    int ans=-1;
+    while(low<=high){
+
+    }
+    return ans;
+
+}
