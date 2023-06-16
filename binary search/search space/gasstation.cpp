b6d29a854511a44c7bd8b106db2d06583816c6de
rishikakinger <rishikakinger@gmail.com>
2023-06-16 21:25:29 +0530

    gas stations done

--- binary search/search space/gasstation.cpp
+++ binary search/search space/gasstation.cpp
@@ -12,3 +12,32 @@
 #include<vector>
 using namespace std;
 
+  int ok(double x, vector<int> &v)
+  {
+    int ret = 0;
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+      ret += ceil((v[i + 1] - v[i]) / x) - 1;//basically floor
+    }
+    return ret;
+  }
+  double findSmallestMaxDist(vector<int> &stations, int K)
+  {
+    double low = 0;
+    int n = stations.size();
+    double high = stations[n - 1] - stations[0];
+    while (high - low >= 1e-6)
+    {
+      double mid = (low + high) / 2.0;
+      int x = ok(mid, stations);
+      if (x > K)
+      {
+        low = mid;
+      }
+      else
+      {
+        high = mid;
+      }
+    }
+    return high;
+  }
