46ecda11f194e532df0953ac1d86be910b58f335
rishikakinger <rishikakinger@gmail.com>
2023-06-25 14:49:19 +0530

    odd even segregate done

--- linked list/single/medium/oddeven.cpp
+++ linked list/single/medium/oddeven.cpp
@@ -1,5 +1,7 @@
 //solution thought of initially wont work- changes relative order
 
+//note: this soln uses no extra space, think about it
+
 #include<bits/stdc++.h>
 #include<iostream>
 #include<vector>
@@ -22,3 +24,44 @@ public:
 };
 
 
+Node* divide(int N, Node *head){
+    Node* even=NULL;
+    Node* odd=NULL;
+    Node* e=NULL;
+    Node* o=NULL;
+
+    while(head){
+        if(head->data%2==0){
+            if(even==NULL){
+                even=head;
+                e=head;
+            }
+            else{
+                e->next=head;
+                e=e->next;
+            }
+        }
+        else{
+            if(odd==NULL){
+                odd=head;
+                o=head;
+
+            }
+            else{
+                o->next=head;
+                o=o->next;
+            }
+
+        }
+        head=head->next;
+
+    }
+    //merging
+    if(e) e->next=odd;
+    if(o) o->next=NULL;
+
+    if(even) return even;
+    return odd;
+
+
+}
