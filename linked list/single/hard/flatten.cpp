30c4ebfeb8648c40f2b06e957d3e8ee15fc73db3
rishikakinger <rishikakinger@gmail.com>
2023-06-28 08:46:37 +0530

    flatten a LL done

---
+++ linked list/single/hard/flatten.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+Node* mergeLists(Node* a, Node* b){
+
+    Node* temp=new Node(0);
+    Node* res=temp;
+
+    while(a!=NULL && b!=NULL){
+        if(a->data < b->data){
+            temp->bottom=a;
+            temp=temp->bottom;
+            a=a->bottom;
+
+        }
+        else{
+            temp->bottom=b;
+            temp=temp->bottom;
+            b=b->bottom;
+        }
+    }
+    if(a) temp->bottom=a;
+    else temp->bottom=b;
+
+    return res->bottom;
+
+}
+
+Node* flattenNode(Node* root){
+
+    if(root == NULL || root->next==NULL) return root;
+
+    root->next=flattenNode(root->next);
+
+    root= mergeLists(root,root->next);
+    return root;
+
+}
