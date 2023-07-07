217baa01cbe27cc4ffad543d86750047f7c683c4
rishikakinger <rishikakinger@gmail.com>
2023-07-07 14:13:49 +0530

    iterative postorder using 2 stacks done

---
+++ binary trees/traversals/8_postorder_2stacks.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<stack>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+vector<int> postorder(Node* root){
+    vector<int> ans;
+    if(root==NULL) return ans;
+    stack<Node*> st1,st2;
+    st1.push(root);
+
+    while(!st1.empty()){
+        root=st1.top();
+        st1.pop();
+        st2.push(root);
+        if(root->left)st1.push(root->left);
+        if(root->right)st1.push(root->right);
+
+
+    }
+    while(!st2.empty()){
+        ans.push_back(st2.top()->data);
+        st2.pop();
+    }
+
+    return ans;
+
+}
