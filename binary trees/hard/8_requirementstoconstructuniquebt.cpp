c4d801c142150a76564823630a7012dbbbdff980
rishikakinger <rishikakinger@gmail.com>
2023-07-12 13:32:47 +0530

    requirements to construct a unique bt (theory) done

---
+++ binary trees/hard/8_requirementstoconstructuniquebt.cpp
@@ -0,0 +1,5 @@
+
+//diff trees can have the same preoder,postorder traversals
+//but inorder and preorder give a unique tree
+
+//inorder is necessary to create a unique binary tree, the other can be pre or post order
