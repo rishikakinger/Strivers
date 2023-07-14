e88018ad61e830c26075d1c685ebffba70cf4495
rishikakinger <rishikakinger@gmail.com>
2023-07-14 13:33:21 +0530

    subset sum 1 done

--- recursion/subsequences pattern/subsetsum1.cpp
+++ recursion/subsequences pattern/subsetsum1.cpp
@@ -1,6 +1,33 @@
 
+//given list of integers, print sums of all subsets in it, in inc order
+
+//brute: generate all subsets(power set)
+
+//better: pick, not pick
+
+//tc is 2^n+2^nlog2^n
+
 #include<bits/stdc++.h>
 #include<iostream>
 #include<vector>
 using namespace std;
 
+void func(int ind,int sum,vector<int>& arr,int N,vector<int>& subSum){
+    if(ind==N){
+        subSum.push_back(sum);
+        return;
+    }
+
+    func(ind+1,sum+arr[ind],arr,N,subSum);
+    func(ind+1,sum,arr,N,subSum);
+
+}
+
+
+vector<int> subsetSum(vector<int> arr,int N){
+    vector<int> subSum;
+    func(0,0,arr,N,subSum);
+    sort(subSum.begin(),subSum.end());
+    return subSum;
+
+}
