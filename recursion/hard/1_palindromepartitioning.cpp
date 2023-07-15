28234882723bf1d25a9a3ae245865c91c16be790
rishikakinger <rishikakinger@gmail.com>
2023-07-15 19:31:01 +0530

    palindrome partitioning done

---
+++ recursion/hard/1_palindromepartitioning.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//brute force: generate all partitions
+
+void func(int index,string s,vector<string>& path,vector<vector<string>>& ans){
+    if(index==s.size()){
+        ans.push_back(path);
+        return;
+    }
+    for(int i=index;i<s.size();i++){
+
+        if(isPalindrome(s,index,i)){
+            path.push_back(s.substr(index,i-index+1));
+            func(i+1,s,path,ans);
+            path.pop_back();
+
+        }
+
+    }
+
+}
+
+bool isPalindrome(string s,int start,int end){
+    while(start<=end){
+        if(s[start++]!=s[end--])return false;
+
+    }
+    return true;
+
+}
+
+
+
+vector<vector<string>> partition(string s){
+    vector<vector<string>> ans;
+
+    vector<string> path;
+    func(0,s,path,ans);
+
+
+    return ans;
+}
+
