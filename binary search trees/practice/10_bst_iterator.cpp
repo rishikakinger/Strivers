af6a3cf0f3de3693a41ab41c70fc84ebf084d3aa
rishikakinger <rishikakinger@gmail.com>
2023-07-22 21:33:52 +0530

    bst iterator

---
+++ binary search trees/practice/10_bst_iterator.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+//sc should be O(H)
+//watch video for intuition
+
+stack<Node*> myStack;
+
+void BSTIterator(Node* root){
+    pushAll(root);
+}
+
+void pushAll(Node* node){
+    for(;node!=NULL;myStack.push(node),node=node->left);
+
+}
+
+bool hasNext(){
+    return !myStack.empty();
+}
+
+int next(){
+    Node* tempNode=myStack.top();
+    myStack.pop();
+    pushAll(tempNode->right);
+    return tempNode->data;
+}
