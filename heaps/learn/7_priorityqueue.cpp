9d7431bb669aee25b372669f5b28f90750686541
rishikakinger <rishikakinger@gmail.com>
2023-07-30 11:24:16 +0530

    kth largest and priority queue done

---
+++ heaps/learn/7_priorityqueue.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//priority queue is nothing but a maxheap
+
+//maxheap
+priority_queue<int> pq;
+
+
+//minheap
+priority_queue<int,vector<int>,greater<int>> minheap;
---
+++ heaps/medium/1_kthlargest.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int> pq;
+    const int n = nums.size();
+    for(int i=0;i<n;++i){pq.push(nums[i]);}
+    while(--k){
+        pq.pop();
+    }
+    return pq.top();
+}
