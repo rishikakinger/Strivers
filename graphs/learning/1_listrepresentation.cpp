0265e55a8a9076b63efdd67774301d9dc4e66a2f
rishikakinger <rishikakinger@gmail.com>
2023-08-11 06:17:53 +0530

    list representation done

--- graphs/learning/1_listrepresentation.cpp
+++ graphs/learning/1_listrepresentation.cpp
@@ -4,4 +4,18 @@ using namespace std;
 
 //cheap
 
- 
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[n+1];//this is a list
+    //if weighted graphs, store pairs
+    
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }//o(2*e)
+
+
+}
