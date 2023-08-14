62e033b819bd22cd8ceeb26a80a0c59f45df2d37
Rishika Kinger <rishikakinger@gmail.com>
2023-08-14 10:08:30 +0530

    detect cycle dfs done

--- graphs/bfs&dfsproblems/5_detectcycledfs.cpp
+++ graphs/bfs&dfsproblems/5_detectcycledfs.cpp
@@ -2,3 +2,38 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+
+//using dfs
+
+//return true if: cycle detected or can go to next neighbour 
+//return false if: dead end 
+
+bool dfs(int node,int parent, int vis[], vector<int> adj[] ){
+    vis[node]=1;
+    for(auto adjacentnode: adj[node]){
+        if(!vis[adjacentnode]){
+            if(dfs(adjacentnode,node,vis,adj)==true){
+               return true; 
+            }
+        }
+        else if(adjacentnode!=parent)return true;
+    }
+    return false;
+
+
+
+}
+
+
+
+bool isCycle(int V,vector<adj> adj[]){
+    int vis[V]={0};
+    for(int i=0;i<V;i++){
+        if(!vis[i]){
+            if(dfs(i,-1,vis,adj)==true)return true;
+        }
+    }
+    return false;
+
+
+}
